981d2d7314ec8cbf4eca8559c46c346d549a0848
mayurjungari <mayurjungari1@gmail.com>
2023-09-16 21:34:31 +0530

    fs modue read write

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+    const reqUrl = req.url;
 
-const server=http.createServer((req,res)=>
-{
-    const reqUrl=req.url;
-    if(reqUrl==='/home')
-    {
-        res.write(' Welcome home')
+    if (reqUrl === '/') {
+        // Read the message from the "message.txt" file
+        fs.readFile('message.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            }
+
+            res.write('<html>');
+            res.write('<head><title> Enter Message</title></head>');
+            res.write('<body>');
+            
+            // Display the message if it exists
+            if (data) {
+                res.write(` ${data}`);
+            }
+
+            res.write('<form action="/message" method="POST"> <input type="text" name="message"> <button type="submit">Send Me</button></form>');
+
+            
+            
+            
+            res.write('</body>');
+            res.write('</html>');
+            return res.end();
+        });
+    } else if (reqUrl === '/message' && req.method === 'POST') {
+        const body = [];
+
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end('Error saving message');
+                } else {
+                    res.statusCode = 302;
+                    res.setHeader('Location', '/');
+                    return res.end();
+                }
+            });
+        });
+    } else {
+        res.write('Welcome home');
         res.end();
     }
-    else if(reqUrl==='/about')
-    {
-        res.write(' Welcome to About Us page')
-        res.end();
-    }
-    else if(reqUrl==='/node')
-    {
-        res.write(' Welcome to my Node Js projecte')
-        res.end();
-    }
-  
 });
+
 server.listen(4000);
