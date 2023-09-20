5fb3164eeecf7f0d0a89eaf62a4d65ba17bfd4fa
mayurjungari <mayurjungari1@gmail.com>
2023-09-21 00:28:56 +0530

    body parser

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 
 const express = require('express');
+const bodyparser = require('body-parser');
 const app=express();
-app.use((req,res,next)=>
+app.use(bodyparser.urlencoded({
+    extended:false,
+}
+));
+
+app.use('/add-product',(req,res,next)=>
 {
-    console.log("in the middle ware")
-    next();
-    
+    console.log("in the another middle ware")
+    res.send('<form action=" /product" method="post"> <input type="text" name="title"> <button type="submit">Add Product</button> </form>');
+
+   
 })
-app.use((req,res,next)=>
+app.post('/product',(req,res,next)=>
+{
+    console.log(req.body)
+    
+    res.redirect('/');
+   
+})
+
+app.use('/',(req,res,next)=>
 {
     console.log("in the another middle ware")
     res.send('<h1>Hello from express</h1>')
+
     
 })
+
 
 
 
